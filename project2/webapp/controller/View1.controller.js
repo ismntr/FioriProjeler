5451c03ea9f1b57a6bd4d2f0c9143c92b3d1b38d
i <ismntr@icloud.com>
2022-11-25 16:47:41 +0300

    text modifier complated

--- project2/webapp/controller/View1.controller.js
+++ project2/webapp/controller/View1.controller.js
@@ -5,25 +5,27 @@ sap.ui.define(
    */
   function (MessageToast, Controller) {
     "use strict";
-
     return Controller.extend("project2.controller.View1", {
       onInit: function () {},
       onPress: function (evt) {
-        var text = this.getView().byId("inputText").getValue();
+        var text =
+          "bu bir deneme metinidir. deneme metini için yazılmıştır. noktadan sonra.başlayan metin, yazılımıştır.";
+        // var text = this.getView().byId("inputText").getValue();
         var array = text.split(".");
-
         for (let i = 0; i < array.length; i++) {       
-            array[i] = " " + array[i].charAt(0).toUpperCase() + array[i].slice(1); 
-         
+          var isBlank = array[i].charAt(0);
+           if ( isBlank == " ") {
+            array[i] = array[i].charAt(0) +
+            array[i].charAt(1).toUpperCase() + 
+            array[i].slice(2).toLowerCase();
+          }  else if (isBlank != " ") {  
+            array[i] = " " + 
+            array[i].charAt(0).toUpperCase() + 
+            array[i].slice(1).toLowerCase();
+          } 
         }
-
         let text2 = array.join(".");
-
-        // var textEdited = array[1].charAt(0).toUpperCase() + array[1].slice(1).toLowerCase();
-
         this.getView().byId("outputText").setValue(text2);
-
-
         MessageToast.show("Duzeltildi");
       },
     });
