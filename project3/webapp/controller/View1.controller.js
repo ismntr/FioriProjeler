515c000c1e4f05c9fc8f5aabba8972d2e317590b
ismet <ismntr@icloud.com>
2022-11-28 02:47:03 +0300

    bu kadar yapabildim

--- project3/webapp/controller/View1.controller.js
+++ project3/webapp/controller/View1.controller.js
@@ -4,107 +4,35 @@ sap.ui.define(
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
   function (MessageToast, Controller) {
-        "use strict";
+    "use strict";
 
-        return Controller.extend("project3.controller.View1", {
-            onInit: function () {},
+    return Controller.extend("project3.controller.View1", {
+      onInit: function () {
+        var result = 0;
 
-            onPress: function (evt) {
-      
-              var text = "Bu bir \"karşılama\" mesajıdır. İkinci mesaj.deneme. üçÜÇÜNCÜ \"mesaj\" da budur.test. "
-              //var text = this.getView().byId("inputText").getValue();
-      
-              var array = text.split(".");
-      
-              for (let i = 0; i < array.length; i++) {
-                var isBlank = array[i].charAt(0);
-                if (isBlank == " ") {
-                  array[i] =
-                    array[i].charAt(0) +
-                    array[i].charAt(1).toUpperCase() +
-                    array[i].slice(2).toLowerCase();
-                } else if (isBlank != " ") {
-                  array[i] =
-                    " " +
-                    array[i].charAt(0).toUpperCase() +
-                    array[i].slice(1).toLowerCase();
-                }
-      
-              }
-              var text = array.join(".");
-               MessageToast.show("Duzeltildi");
-      
-      /*
-             var arrayQM = text2.split('\"');
-              for (let i = 0; i < arrayQM.length; i++) {
-                if (i % 2 == 1) {
-                  arrayQM[i] = arrayQM[i].toUpperCase();
-                }
-              }     
-               text2 = arrayQM.join("\"");  */
-      
-      
-      
-              this.getView().byId("outputText").setValue(text); 
-            },
-            onPress2: function (evt) {
-              var text = this.getView().byId("inputText").getValue();
-      
-      
-         
-              
-              var arrayQM = text.split('\"');
-              
-              for (let i = 0; i < arrayQM.length; i++) {
-                if (i % 2 == 1) {
-                  arrayQM[i] = arrayQM[i].toUpperCase();
-                }
-                // text2 = arrayQM[i-1] + "\""+ arrayQM[i] + "\""+ arrayQM[i+1];
-              }     
-               text = arrayQM.join("\"");
-      
-              this.getView().byId("outputText").setValue(text);
-                    MessageToast.show("Büyütüldü");
-            },
-            onPress3: function (evt) {
-      
-              //var text = "Bu bir \"karşılama\" mesajıdır. İkinci mesaj.deneme. üçÜÇÜNCÜ \"mesaj\" da budur.test. "
-              var text = this.getView().byId("inputText").getValue();
-      
-              var array = text.split(".");
-      
-              for (let i = 0; i < array.length; i++) {
-                var isBlank = array[i].charAt(0);
-                if (isBlank == " ") {
-                  array[i] =
-                    array[i].charAt(0) +
-                    array[i].charAt(1).toUpperCase() +
-                    array[i].slice(2).toLowerCase();
-                } else if (isBlank != " ") {
-                  array[i] =
-                    " " +
-                    array[i].charAt(0).toUpperCase() +
-                    array[i].slice(1).toLowerCase();
-                }
-      
-              }
-              var text2 = array.join(".");
-               MessageToast.show("Duzeltildi");
-      
-      
-             var arrayQM = text2.split('\"');
-              for (let i = 0; i < arrayQM.length; i++) {
-                if (i % 2 == 1) {
-                  arrayQM[i] = arrayQM[i].toUpperCase();
-                }
-              }     
-               text2 = arrayQM.join("\"");  
-      
-      
-      
-              this.getView().byId("outputText").setValue(text2); 
-              MessageToast.show("Düzeltildi ve Büyütüldü");
-      
-            }
-        });
+        this.getView().byId("inputText").setValue(result);
+      },
+
+      onPress: function (result, text) {
+        var text;
+        var operator;
+        result = this.getView().byId("inputText").getValue(result);
+        var button = this.getView().byId("add").clicked;
+
+        if (this.getView().byId("add").clicked) {
+          return (operator = "+");
+        }
+        if (this.getView().byId("1").clicked) {
+          return (text = 1);
+        }
+        if (this.getView().byId("2").clicked) {
+          return (text = 2);
+        }
+
+        var result = result + operator + text;
+        this.getView().byId("inputText").setValue(result);
+        console.log(result);
+      },
     });
+  }
+);
