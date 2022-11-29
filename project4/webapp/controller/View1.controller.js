53506d6c934e0a0947234a286a0b749129cb48be
i <ismntr@icloud.com>
2022-11-29 19:01:04 +0300

    Object ödevi çalışır durumda

--- project4/webapp/controller/View1.controller.js
+++ project4/webapp/controller/View1.controller.js
@@ -6,31 +6,37 @@ sap.ui.define(
   function (MessageToast, Controller) {
     "use strict";
 
-    var result;
-    var buttonText;
-    var Text;
-
-    var operator;
-    var input1;
-    var input2;
+    var result = 0;
+    var buttonText = "";
+    var operator ="";
+    var input1 =0;
+    var input2 =0;
+    var inputText ="";
 
     return Controller.extend("project4.controller.View1", {
       onInit: function () {
         this.getView().byId("inputText").setValue(0);
       },
+
+      
       onPress: function (evt) {
         buttonText = evt.getSource().getText();
-        Text = parseFloat(this.getView().byId("inputText").getValue());
-        if (!isNaN(buttonText)) {
+
+       var Text = parseFloat(this.getView().byId("inputText").getValue());
           if (Text === 0) {
             Text = buttonText;
-            this.getView().byId("inputText").setValue(Text);
           } else {
             Text = Text + buttonText.toString();
-            this.getView().byId("inputText").setValue(Text);
           }
-        }
+          this.getView().byId("inputText").setValue(Text);
       },
+
+
+
+
+
+
+
       onPress2: function (evt) {
         buttonText = evt.getSource().getText();
         input1 = parseFloat(this.getView().byId("inputText").getValue());
@@ -38,13 +44,21 @@ sap.ui.define(
           operator = buttonText;
           this.getView().byId("inputText").setValue(0);
         }
+
       },
+
+
+
+
+
+
+
       onPress3: function (evt) {
         buttonText = evt.getSource().getText();
         input2 = parseFloat(this.getView().byId("inputText").getValue());
         if (buttonText == "=") {
           if (operator == "+") {
-            result = input1 + input2;
+            result = Number(input1) + Number(input2) ;
           } else if (operator == "-") {
             result = input1 - input2;
           } else if (operator == "*") {
@@ -54,79 +68,18 @@ sap.ui.define(
           }
           this.getView().byId("inputText").setValue(result);
         }
-      },onPressAC: function (evt) {
+      },
+      
+      
+      
+      
+      
+      onPressAC: function (evt) {
         buttonText = evt.getSource().getText();
         if (buttonText == "AC") {
           this.getView().byId("inputText").setValue(0);
         }
       },
-
-      /*       onPress: function (evt) {
-        buttonText = parseFloat(evt.getSource().getText());
-        this.getView().byId("inputText").setValue(inputText);
-        inputText = parseFloat(this.getView().byId("inputText").getValue());
-
-        if ( !isNaN(buttonText)) {
-          //sayıysa sayılara atama yapılacak
-          if (number1 == null ) {
-            number1 = buttonText;
-            number1 = inputText.toString() + number1.toString();
-            this.getView().byId("inputText").setValue(inputText); 
-
-          }else if (number1 != null ) {
-            number2 = buttonText;
-            inputText += number2;
-            this.getView().byId("inputText").setValue(inputText);  
-            
-          }
-        } else if ( isNaN(buttonText))
-        {
-          //operatöre atama yapılacak
-        }
-
-        MessageToast.show(inputText);
-      }, */
-
-      /*       onPress2: function (evt) {
-        var inputText2 = this.getView().byId("inputText").getValue();
-        this.number3 = inputText2
-        var buttonText = evt.getSource().getText();
-        var operator;
-        if (buttonText == "+") {
-          var resultArray = inputText2.split("+");
-          for (let i = 0; i < resultArray.length; i++) {
-            resultArray[i] += resultArray[i];           
-          }
-          operator = "+";
-        } else if (buttonText == "-") {
-          operator = "-";
-        } else if (buttonText == "*") {
-          operator = "*";
-        } else if (buttonText == "/") {
-          operator = "/";
-        } else {
-          result = "Yanlış işlem";
-        }
-        inputText2 += operator;
-
-         this.getView().byId("inputText").setValue(inputText2);
-
-        MessageToast.show(operator);
-      },
-
-      onPress3: function () {
-
-        var inputText3 = this.getView().byId("inputText").getValue();
-
-        var result = inputText3.split("\"")
-        var sonuc = result[0];
-        result = sonuc;
-         
-
-        this.getView().byId("inputText").setValue(number3);
-
-      },
-       */
     });
   }
 );
--- project5object/webapp/controller/View1Obj.controller.js
+++ project5object/webapp/controller/View1Obj.controller.js
@@ -6,10 +6,87 @@ sap.ui.define([
      */
     function (Controller) {
         "use strict";
-
+        var result;
+        var buttonText;
+        var Text;
+        var operator;
+        var input1;
+        var input2;
+        var inputText1;
+        var inputText2;
+        var outputText;
+        var islem = {
+        "inputText1":0,
+        "inputText2":0,
+        "operator":"",
+        "result":0};
+        var islemler=[];
         return Controller.extend("project5object.controller.View1Obj", {
             onInit: function () {
+            },
+            onPress: function (evt) {
+              buttonText = evt.getSource().getText();
+              Text = parseFloat(this.getView().byId("inputText").getValue());
+              if (!isNaN(buttonText)) {
+                if (Text === 0) {
+                  Text = buttonText;
+                  this.getView().byId("inputText").setValue(Text);
+                } else {
+                  Text = Text + buttonText.toString();
+                  this.getView().byId("inputText").setValue(Text);
+                }
+              }
+            },
+
+            onPress2: function (evt) {
+              operator = evt.getSource().getText();
+              
+            },
+
+            onPressEkle: function (evt) {
+              inputText1 = parseFloat(this.getView().byId("inputText").getValue());
+              inputText2 = parseFloat(this.getView().byId("inputText2").getValue());
+                islem= {
+                  "inputText1":inputText1,
+                  "inputText2":inputText2,
+                  "operator":operator,
+                  "result": result
+                }
+
+                console.log(islem);
+
+              islemler.push(islem)
+              this.getView().byId("inputText").setValue("");
+              this.getView().byId("inputText2").setValue("");      
+            },
+
+            onPressHesapla: function (evt) {
+              for (let i = 0; i < islemler.length; i++) {     
+                input1 = islemler[i].inputText1;
+                input2 = islemler[i].inputText2;
+                operator = islemler[i].operator;
+                result = islemler[i].result;                
+                if (operator == "+") {
+                  result = input1 + input2;
+                } else if (operator == "-") {
+                  result = input1 - input2;
+                } else if (operator == "*") {
+                  result = input1 * input2;
+                } else if (operator == "/") {
+                  result = input1 / input2;
+                }
+                islemler[i].result = result;
+                var yazdir = "İlk Girilen Sayı= '" + islemler[i].inputText1 + "', İkinci Girilen Sayı= '" + islemler[i].inputText2 + "', İşlem Operatörü= '" + operator + "', Sonuç: '" + result + "'\n";
+                if (!outputText) {
+                  outputText = yazdir;
+                } else {
+                  outputText +=  yazdir;
+                }
+                this.getView().byId("outputText").setValue(outputText);           
+              }
+              console.log(islemler);
 
             }
-        });
-    });
+          });
+        }
+      );
