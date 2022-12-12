4f6ddbffb9cbe3e73518dc2c0feb677f66ab2e00
i <ismntr@icloud.com>
2022-12-12 16:57:16 +0300

    delete bug var

--- project11fragment/webapp/controller/View1fragment.controller.js
+++ project11fragment/webapp/controller/View1fragment.controller.js
@@ -15,7 +15,7 @@ sap.ui.define(
       onInit: function () {
         this.oModel = this.getOwnerComponent().getModel("mainModel");
         this.i18n = this.getOwnerComponent().getModel("i18n");
-
+        this.oModel.setProperty("/mode", "SingleSelectMaster");
         this.oModel.setProperty("/contact", {});
         this.oModel.setProperty("/contacts", []);
 
@@ -29,28 +29,56 @@ sap.ui.define(
         };
         this.oModel.setProperty("/contact/", acilis);
       },
-      onPressNew: function(oEvent){
-        this._getDialogContact("project11fragment.view.fragments.NewContact").open();
-
+      onPressNew: function (oEvent) {
+        this._getDialogContact(
+          "project11fragment.view.fragments.NewContact"
+        ).open();
       },
-      onPressCloseNewContact: function(oEvent){
-        this._getDialogContact("project11fragment.view.fragments.NewContact").close();
+      onPressCloseNewContact: function (oEvent) {
+        this._getDialogContact(
+          "project11fragment.view.fragments.NewContact"
+        ).close();
+      },
+      delete: function (oEvent) {
+        if (this.oModel.getProperty("/mode") == "Delete") {
+          this.oModel.setProperty("/mode", "SingleSelectMaster");
+        } else if (this.oModel.getProperty("/mode") == "SingleSelectMaster") {
+          this.oModel.setProperty("/mode", "Delete");
+        }
+      },
+      handleDelete: function (oEvent) {
+        var contacts = this.oModel.getProperty("/contacts");
+        var oList = oEvent.getSource(),
+				sPath =  oEvent.getParameter("listItem").getBindingContextPath(),
+				oItem =  this.oModel.getProperty(sPath)
 
+        contacts.splice(contacts.indexOf(oItem));
+
+        this.oModel.setProperty("/contacts", contacts);
       },
       _getDialogContact: function (fragName) {
         if (!this._oDialogContact) {
-            this._oDialogContact = sap.ui.xmlfragment(fragName, this);
-            this.getView().addDependent(this._oDialogContact);
+          this._oDialogContact = sap.ui.xmlfragment(fragName, this);
+          this.getView().addDependent(this._oDialogContact);
         }
         return this._oDialogContact;
-    },
-
-
+      },
 
       onPressSave: function (evt) {
         var contact = this.oModel.getProperty("/contact");
         var contacts = this.oModel.getProperty("/contacts");
-        //validations
+
+        if (
+          !contact.id ||
+          !contact.name ||
+          !contact.surname ||
+          !contact.phone ||
+          !contact.birthday ||
+          !contact.address
+        ) {
+          this.showMessage(this.i18n.getProperty("inputError"));
+          return;
+        }
 
         if (this.oModel.getProperty("/inputEnabled") == false) {
           for (let i = 0; i < contacts.length; i++) {
@@ -68,18 +96,6 @@ sap.ui.define(
               return;
             }
           }
-        }
-
-        if (
-          !contact.id ||
-          !contact.name ||
-          !contact.surname ||
-          !contact.phone ||
-          !contact.birthday ||
-          !contact.address
-        ) {
-          this.showMessage(this.i18n.getProperty("inputError"));
-          return;
         }
 
         if (contact.id.length != 11) {
@@ -107,7 +123,11 @@ sap.ui.define(
         this.onPressShow();
         this.onPressClear();
         this.oModel.setProperty("/inputEnabled", true);
-        this._getDialogContact("project11fragment.view.fragments.NewContact").close();
+        this.oModel.setProperty("/mode", "SingleSelectMaster");
+
+        this._getDialogContact(
+          "project11fragment.view.fragments.NewContact"
+        ).close();
       },
 
       onPressShow: function (evt) {
@@ -126,14 +146,12 @@ sap.ui.define(
             "Telefon: " +
             showInput.phone +
             "\n" +
-
             "Doğrum Günü: " +
             showInput.birthday.getDay() +
             "." +
             (showInput.birthday.getMonth() + 1) +
             "." +
             showInput.birthday.getFullYear() +
-
             "\n" +
             "Adress: " +
             showInput.address
@@ -158,7 +176,7 @@ sap.ui.define(
 
         var ItemAssign = Object.assign({}, selectedItem);
         this.oModel.setProperty("/contact", ItemAssign);
-
+        this.oModel.setProperty("/mode", "SingleSelectMaster");
         this.inputIdDisabled();
       },
       inputIdDisabled: function () {
