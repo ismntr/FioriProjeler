7bbee7b86c361a2edbe1866b7d9dcc0e44af6438
i <ismntr@icloud.com>
2023-01-04 18:44:08 +0300

    fragment düzenlendi

--- project16readfilter/webapp/controller/View1readFilter.controller.js
+++ project16readfilter/webapp/controller/View1readFilter.controller.js
@@ -13,7 +13,7 @@ sap.ui.define(
 
     return Controller.extend("project16readfilter.controller.View1readFilter", {
       formatter: {
-        formatActive: function (active) {
+/*         formatActive: function (active) {
           return active ? "Aktif" : "Pasif";
         },
         formatGender: function (gender) {
@@ -44,7 +44,7 @@ sap.ui.define(
               break;
           }
           return district;
-        },
+        }, */
       },
 
       onInit: function () {
@@ -53,7 +53,7 @@ sap.ui.define(
         this.oModel.setProperty("/mode", "SingleSelectMaster");
         this.oModel.setProperty("/contact", {});
         this.oModel.setProperty("/contacts", []);
-
+/* 
         var acilis = {
           id: "11111111111",
           name: "isim",
@@ -160,49 +160,38 @@ sap.ui.define(
           },
         ]);
 
-        this.oModel.setProperty("/contact/", acilis);
-        console.log("/contact", this.oModel.getProperty("/contact"));
+        this.oModel.setProperty("/contact/", acilis); 
+        console.log("/contact", this.oModel.getProperty("/contact"));*/
+
+        this.oDataModel = this.getOwnerComponent().getModel();
+        this.oModel = this.getOwnerComponent().getModel("mainModel");
+        var that = this;
+        sap.ui.core.BusyIndicator.show(0);
+        this.getOwnerComponent().getModel().read("/Orders", {
+            success: function (oData) {
+              that.oModel.setProperty("/Orders", oData.results);
+              sap.ui.core.BusyIndicator.hide(0);
+            },
+            error: function (oResponse) {
+              debugger;
+            },
+          });
+
+          this.getOwnerComponent().getModel().read("/Order_Details", {
+            success: function (oData) {
+              that.oModel.setProperty("/Order_Details", oData.results);
+              sap.ui.core.BusyIndicator.hide(0);
+            },
+            error: function (oResponse) {
+              debugger;
+            },
+          });
+
+
       },
 
-      onCountryChange: function (oEvent) {
-        var selectedCountryKey = oEvent.getSource().getSelectedKey();
-        // var cities = this.oModel.getProperty("/cities").filter(function (city) {
-        //   return city.country === selectedCountryKey;
-        // });
-        // this.getView()
-        //   .getModel("mainModel")
-        //   .setProperty("/filteredCities", cities);
 
-        var oBinding = sap.ui.getCore().byId("idCity").getBinding("items");
-        var aFilter = new sap.ui.model.Filter({
-          filters: [
-            new sap.ui.model.Filter(
-              "country",
-              sap.ui.model.FilterOperator.EQ,
-              selectedCountryKey
-            ),
-          ],
-        });
-
-        oBinding.filter(aFilter);
-        sap.ui.getCore().byId("idCity").setSelectedKey();
-      },
-      onCountryCity: function (oEvent) {
-        var selectedCityKey = oEvent.getSource().getSelectedKey();
-        var oBinding = sap.ui.getCore().byId("idDistrict").getBinding("items");
-        var aFilter = new sap.ui.model.Filter({
-          filters: [
-            new sap.ui.model.Filter(
-              "city",
-              sap.ui.model.FilterOperator.EQ,
-              selectedCityKey
-            ),
-          ],
-        });
-
-        oBinding.filter(aFilter);
-        sap.ui.getCore().byId("idDistrict").setSelectedKey();
-      },
+ 
 
       onNameSurnameSearch: function (oEvent) {
         var sSearchValue = oEvent.getParameter("value");
@@ -233,23 +222,23 @@ sap.ui.define(
         var oTable = this.byId("myTable");
 
         if (oToggleButton.getPressed()) {
-          var oSorter = new sap.ui.model.Sorter("id", false);
+          var oSorter = new sap.ui.model.Sorter("OrderID", false);
           oTable.getBinding("items").sort(oSorter);
         } else {
-          var oSorter = new sap.ui.model.Sorter("id", true);
+          var oSorter = new sap.ui.model.Sorter("OrderID", true);
           oTable.getBinding("items").sort(oSorter);
         }
       },
 
       onPressNew: function (oEvent) {
         this._getDialogContact(
-          "project16readfilter.view.fragments.NewContact"
+          "project16readfilter.view.fragments.OrderDetail"
         ).open();
       },
 
       onPressCloseNewContact: function (oEvent) {
         this._getDialogContact(
-          "project16readfilter.view.fragments.NewContact"
+          "project16readfilter.view.fragments.OrderDetail"
         ).close();
       },
 
@@ -278,113 +267,6 @@ sap.ui.define(
         return this._oDialogContact;
       },
 
-      onPressSave: function (evt) {
-        var contact = this.oModel.getProperty("/contact");
-        var contacts = this.oModel.getProperty("/contacts");
-
-        if (
-          !contact.id ||
-          !contact.name ||
-          !contact.surname ||
-          !contact.phone ||
-          !contact.birthday ||
-          !contact.address ||
-          !contact.selectedCountry ||
-          !contact.selectedCity ||
-          !contact.selectedDistrict
-        ) {
-          this.showMessage(this.i18n.getProperty("inputError"));
-          return;
-        }
-
-        if (this.oModel.getProperty("/inputEnabled") == false) {
-          for (let i = 0; i < contacts.length; i++) {
-            if (contacts[i].id === contact.id) {
-              contacts[i].name = contact.name;
-              contacts[i].surname = contact.surname;
-              contacts[i].phone = contact.phone;
-              contacts[i].birthday = contact.birthday;
-              contacts[i].address = contact.address;
-              contacts[i].gender = contact.gender;
-              contacts[i].active = contact.active;
-              contacts[i].selectedCountry = contact.selectedCountry;
-              contacts[i].selectedCity = contact.selectedCity;
-              contacts[i].selectedDistrict = contact.selectedDistrict;
-
-              this.oModel.setProperty("/inputEnabled", true);
-
-              this.byId("myTable").removeSelections(true);
-              this.onPressCloseNewContact();
-
-              return;
-            }
-          }
-        }
-
-        if (contact.id.length != 11) {
-          this.showMessage(this.i18n.getProperty("idLengthError"));
-          return;
-        }
-
-        for (let i = 0; i < contacts.length; i++) {
-          if (contacts[i].id === contact.id) {
-            this.showMessage(this.i18n.getProperty("idError"));
-            return;
-          }
-        }
-
-        for (let i = 0; i < contacts.length; i++) {
-          if (contacts[i].id === contact.id) {
-            contacts[i].name = this.oModel.getProperty("/contact/name");
-            return;
-          }
-        }
-
-        var contactsAssign = Object.assign({}, contact);
-        contacts.push(contactsAssign);
-        this.oModel.setProperty("/contacts", contacts);
-        // this.onPressShow();
-        this.onPressClear();
-        this.oModel.setProperty("/inputEnabled", true);
-        this.oModel.setProperty("/mode", "SingleSelectMaster");
-
-        this.onPressCloseNewContact();
-      },
-
-      onPressShow: function (evt) {
-        var showInput = this.oModel.getProperty("/contact");
-
-        MessageToast.show(
-          "TC: " +
-            showInput.id +
-            "\n" +
-            "Adı: " +
-            showInput.name +
-            "\n" +
-            "Soyadı: " +
-            showInput.surname +
-            "\n" +
-            "Telefon: " +
-            showInput.phone +
-            "\n" +
-            "Doğrum Günü: " +
-            showInput.birthday.getDay() +
-            "." +
-            (showInput.birthday.getMonth() + 1) +
-            "." +
-            showInput.birthday.getFullYear() +
-            "\n" +
-            "Adress: " +
-            showInput.address
-        );
-
-        // console.log(this.oModel.getProperty("/contacts"));
-      },
-
-      onPressClear: function (evt) {
-        this.oModel.setProperty("/contact", {});
-        this.oModel.setProperty("/inputEnabled", true);
-      },
 
       showMessage: function (message) {
         MessageBox.error(message);
@@ -394,7 +276,7 @@ sap.ui.define(
         var selectedPath = oEvent.getSource().getSelectedContexts()[0].sPath;
         var selectedItem = this.oModel.getProperty(selectedPath);
         var ItemAssign = Object.assign({}, selectedItem);
-        this.oModel.setProperty("/contact", ItemAssign);
+        this.oModel.setProperty("/order", ItemAssign);
         this.oModel.setProperty("/mode", "SingleSelectMaster");
         this.inputIdDisabled();
         this.onPressNew();
@@ -402,31 +284,6 @@ sap.ui.define(
 
       inputIdDisabled: function () {
         this.oModel.setProperty("/inputEnabled", false);
-      },
-
-      onChange: function (oEvent) {
-        var oInput = oEvent.getSource();
-        var sValue = oInput.getValue();
-
-        if (sValue.length === 0) {
-          oInput.setValueState("Error");
-        } else {
-          oInput.setValueState("None");
-        }
-      },
-      onChange2: function (oEvent) {
-        var oInput = oEvent.getSource();
-        var sValue = oInput.getValue();
-        var sPath = oInput.getBindingContext("mainModel").getPath();
-        this.getModel("mainModel").setProperty(sPath, sValue);
-      },
-      onGenderSelect: function (oEvent) {
-        var oRadioButtonGroup = oEvent.getSource();
-        var oSelectedButton = oRadioButtonGroup.getSelectedButton();
-        var sValue = oSelectedButton.getText();
-        this.getView()
-          .getModel("mainModel")
-          .setProperty("/contact/gender", sValue);
       },
     });
   }
