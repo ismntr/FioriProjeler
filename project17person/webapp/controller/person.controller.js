e7bcbfa59fc5da79c24bb2f13f7bc7ea49e53b39
i <ismntr@icloud.com>
2023-01-18 12:20:17 +0300

    js kopyalandı düzenlenecek butonlar eklendi

--- project17person/webapp/controller/person.controller.js
+++ project17person/webapp/controller/person.controller.js
@@ -8,13 +8,10 @@ sap.ui.define(
 
     return Controller.extend("project17person.controller.person", {
       onInit: function () {
-
         this.oMainModel = this.getOwnerComponent().getModel("mainModel");
         this.oDataModel = this.getOwnerComponent().getModel();
-        console.log( this.oMainModel);
+        console.log(this.oMainModel);
         console.log(this.oDataModel);
-
-
 
         var that = this;
         sap.ui.core.BusyIndicator.show(0);
@@ -27,8 +24,120 @@ sap.ui.define(
               console.log(that.oMainModel);
             },
           });
-        
+      },
+      onCalis: function (oEvent) {
+        var oData = {
+          EventTime: "",
+          UserId: "",
+          Works: "",
+          YWorks: "",
+          Basla: "",
+        };
 
+        oData.EventTime = this._oview
+          .getModel("viewModel")
+          .getProperty("/EventTime");
+        oData.UserId = this._oview.getModel("viewModel").getProperty("/UserId");
+        oData.Basla = this._oview.getModel("viewModel").getProperty("/Basla");
+        oData.Works = this._oview.getModel("viewModel").getProperty("/Works");
+        oData.YWorks = this._oview.getModel("viewModel").getProperty("/YWorks");
+
+        var pressed_btn = oEvent.getSource().getText();
+        // var pressed_btn = oEvent.getSource().getId();
+        if (pressed_btn === "Çalış") {
+          if (oData.Works === "") {
+            MessageToast.show("Yapılması planlanlanan işleri giriniz...");
+            return;
+          }
+        } else {
+          if (oData.YWorks === "") {
+            MessageToast.show("Yapılan işleri giriniz...");
+            return;
+          } else {
+            oData.Works = oData.YWorks;
+          }
+        }
+        debugger;
+        //Kayıt tipi
+        oData.Basla = "";
+        if (pressed_btn === "Çalış") {
+          oData.Basla = "B";
+          mesaj = "Uzaktan çalışma başlatıldı...";
+
+          this.getView().byId("Btn1").setEnabled(false);
+          this.getView().byId("Btn2").setVisible(false);
+          this.getView().byId("Btn3").setVisible(false);
+          this.getView().byId("Pworks").setEnabled(false);
+          this.getView().byId("Yworks").setEnabled(false);
+        } else {
+          if (pressed_btn === "Çalışmayı Bitir") {
+            oData.Basla = "F";
+            mesaj = "Uzaktan çalışma bitirildi...";
+            this.getView().byId("Btn1").setEnabled(false);
+            this.getView().byId("Btn2").setEnabled(false);
+            this.getView().byId("Btn3").setEnabled(false);
+            this.getView().byId("Pworks").setEnabled(false);
+            this.getView().byId("Yworks").setEnabled(false);
+          } else {
+            mesaj = "Uzaktan çalışma güncellendi...";
+          }
+        }
+
+        sap.ui.core.BusyIndicator.show(0);
+        this.getView()
+          .getModel()
+          .create("/PersonelUzaktanCalismaSet", oData, {
+            success: function (oData) {
+              sap.ui.core.BusyIndicator.hide();
+
+              //Final mesaj
+              sap.m.MessageBox.information(mesaj, {
+                title: "BİLGİ...",
+                onClose: function (oAction) {
+                  if (oAction === "OK") {
+                    // if (pressed_btn !== "" ) {
+                    // window.close();
+                    // }
+                  }
+                },
+                styleClass: "",
+                actions: sap.m.MessageBox.Action.OK,
+                emphasizedAction: sap.m.MessageBox.Action.OK,
+                initialFocus: null,
+                textDirection: sap.ui.core.TextDirection.Inherit,
+              });
+            },
+
+            error: function (oError) {
+              sap.ui.core.BusyIndicator.hide();
+              return;
+            },
+          });
+      },
+
+      onIptal: function () {
+        if (init_hata === "X") {
+          // window.open("", '_self').window.close();
+
+          window.close();
+        } else {
+          sap.m.MessageBox.confirm("Onaylıyor musunuz?", {
+            title: "Programdan Çıkılacak...",
+            onClose: function (oAction) {
+              if (oAction === "OK") {
+                close();
+              }
+            },
+            styleClass: "",
+            actions: [
+              sap.m.MessageBox.Action.OK,
+              sap.m.MessageBox.Action.CANCEL,
+            ],
+            emphasizedAction: sap.m.MessageBox.Action.OK,
+            initialFocus: null,
+            textDirection: sap.ui.core.TextDirection.Inherit,
+          });
+        }
       },
     });
   }
